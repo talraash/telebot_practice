452aa4f17f3ce53a8600e729e4066fedcdf6c59b
talraash <talraash@gmail.com>
2021-12-29 00:48:57 +0300

    Dirty code, need refactoring. When bot get text, search book on fantlab and return first link

--- telebot_practice.py
+++ telebot_practice.py
@@ -1,4 +1,6 @@
 import telebot
+import requests as req
+from bs4 import BeautifulSoup
 from datetime import datetime
 from token_telegram import token
 
@@ -11,6 +13,9 @@ def welcome(message):
     bot.reply_to(message, f"Hi, {message.from_user.username}!")
 
 
+
+
+
 #Handle sticker and returne it with emoji
 @bot.message_handler(content_types=['sticker'])
 def stiker(message):
@@ -20,11 +25,26 @@ def stiker(message):
 
 # handle text send echo reply and save it to text_log
 @bot.message_handler(func=lambda message: True)  
-def echo_all(message):
-    bot.reply_to(message, message.text)
-    with open('text_log', 'a') as text:
-        text.write(datetime.utcfromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S '))
-        text.write(f"You message: '{message.text}' \n")
-    
+def book_find(message):
+    request = req.get("https://fantlab.ru/searchmain?searchstr=" + message.text)
+#    with open('fantlab_log', 'a') as text:
+#    text.write(request.text)
+
+    soup = BeautifulSoup(request.text, 'html.parser')
+    result = soup.find_all(class_='search-block works')
+
+    for element in result:
+        title = element.find_all(class_='title')
+        t = str(title[0].find('a'))
+        t = "fantlab.ru" + t[t.find('/'):t.rfind('"')]
+    bot.reply_to(message, t)
+
+
+
+"""    bot.reply_to(message, message.text)
+       with open('text_log', 'a') as text:
+            text.write(datetime.utcfromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S '))
+            text.write(f"You message: '{message.text}' \n")
+"""    
     
 bot.polling(none_stop=True, interval=1)
