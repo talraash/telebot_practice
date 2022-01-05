1dcb9a4318cd7cf00ca30d2ff8b0871797d0fa89
talraash <talraash@gmail.com>
2022-01-05 12:57:30 +0300

    fixing for parsing

--- aiogram_practice.py
+++ aiogram_practice.py
@@ -1,11 +1,10 @@
-import asyncio
-
 from aiogram import Bot, Dispatcher, executor, types
 from telegram_token import token
 import requests as req
 import aiohttp
 from aiogram.utils.emoji import emojize
 from bs4 import BeautifulSoup
+import lxml.html
 
 bot = Bot(token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
@@ -33,32 +32,23 @@ async def reply2(message: types.Message):
 async def reply2(message: types.Message):
     await message.answer(f'Hi, {message.chat.username} {emojize(":wink:")} Just send /cat and you got some reward')
 
-
+url = []
 @dp.message_handler()
 async def get_fantlab_content(message: types.Message):
     async with aiohttp.ClientSession() as session:
         async with session.get('https://fantlab.ru/searchmain?searchstr=' + message.text) as respond:
             text = await respond.read()
     soup = BeautifulSoup(text.decode('utf-8'), 'lxml')
-    info = soup.find_all(class_='search-block works')
-    for elements in info:
-        title = elements.find_all_next(class_='title')
-        if len(title) > 3:
-            number_elements = 3
-        else:
-            number_elements = len(title)
-        for i in title:
-            t = str(i.find('a'))
-            t = "fantlab.ru" + t[t.find('/'):t.rfind('"')]
-            await message.answer(t)
-            number_elements -= 1
-            if number_elements == 0:
-                break
+    info = soup.find(class_='search-block works')
+    rathings = info.select('big', limit=3)
+    autor = info.find_all(class_='autor', limit=3)
+    title = info.find_all(class_='title', limit=3)
+    for i in title:
+        t = str(i.find('a'))
+        url.append("fantlab.ru" + t[t.find('/'):t.rfind('"')])
+    for i in range(3):
+        print(f'Рейтинг :{rathings[i]} \n Автор :{autor[i]},\n Произведение :{title[i]} \n Ссылка: {url[i]}')
 
-
-
-
-#dp.register_message_handler(reply2, commands='start')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
